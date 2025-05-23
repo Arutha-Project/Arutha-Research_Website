7df0e99afed644ff05d5efec9d782212c2ef2c68
OvindaNamal <j.m.ovinda@gmail.com>
2025-05-24 00:46:37 +0530

    Modify Time Lines

--- src/Pages/TimeLines.tsx
+++ src/Pages/TimeLines.tsx
@@ -39,32 +39,32 @@ function TimeLines() {
             description: 'Progress Presentation II reviews the 90% completetion status demonstration of the project. Along with a Poster presesntation which describes the project as a whole.'
         },
         {
-            name: 'Website Assessment',
-            date: 'May 2026',
-            progress: '57%',
-            mark: '2',
-            description: 'The Website helps to promote our research project and reveals all details related to the project.'
-        },
-        {
-            name: 'Logbook',
-            date: 'May 2023',
-            progress: '60%',
-            mark: '3',
-            description: 'Status of the project is validated through the Logbook. This also includes, Status documents 1 & 2.'
-        },
-        {
             name: 'Final Report',
-            date: 'May 2024',
+            date: 'April 2025',
             progress: '80%',
             mark: '20',
             description: 'Final Report evalutes the completed project done throughout the year. Marks mentioned below includes marks for Individual & group reports and also Final report.'
         },
         {
             name: 'Final Presentation & Viva',
-            date: 'May 2024',
+            date: 'May 2025',
             progress: '100%',
             mark: '20',
             description: 'Viva is held individually to assess each members contribution to the project.'
+        },
+        {
+            name: 'Website Assessment',
+            date: 'June 2025',
+            progress: '57%',
+            mark: '2',
+            description: 'The Website helps to promote our research project and reveals all details related to the project.'
+        },
+        {
+            name: 'Logbook',
+            date: 'June 2025',
+            progress: '60%',
+            mark: '3',
+            description: 'Status of the project is validated through the Logbook. This also includes, Status documents 1 & 2.'
         }
     ]
 
