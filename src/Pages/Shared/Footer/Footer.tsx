1b0d78b6827a5d706a3b7a4491080061412d5bdc
ushandilusha <ushandilusha10@gmail.com>
2025-05-23 20:50:56 +0530

    fixed contact us and footer

--- src/Pages/Shared/Footer/Footer.tsx
+++ src/Pages/Shared/Footer/Footer.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import './Footer.css';
 
 const Footer = () => {
@@ -20,11 +19,11 @@ const Footer = () => {
                                     book system is to provide visually impaired university students in Sri Lanka
                                 </p>
                                 <div className="social mt-2 mb-3">
-                                    <i className="fa fa-facebook-official fa-lg" onClick={() => { window.location.replace("https://www.facebook.com/profile.php?id=100007444869672&mibextid=LQQJ4d") }}></i>
-                                    <i className="fa fa-instagram fa-lg" onClick={() => { window.location.replace("https://instagram.com/k_a_m_i_n_d_u_") }}></i>
-                                    <i className="fa fa-twitter fa-lg" onClick={() => { window.location.replace("https://twitter.com/k_a_m_i_n_d_u_") }}></i>
-                                    <i className="fa fa-linkedin-square fa-lg" onClick={() => { window.location.replace("https://www.linkedin.com/in/kamindu-gayantha-4693661b5") }}></i>
-                                    <i className="fa fa-github" onClick={() => { window.location.replace("https://github.com/Kamindu99") }}></i>
+                                    <i className="fa fa-facebook-official fa-lg" onClick={() => { window.location.replace("https://www.facebook.com/ushan.dilusha.10") }}></i>
+                                    <i className="fa fa-instagram fa-lg" onClick={() => { window.location.replace("https://www.instagram.com/ushan_dilusha_/") }}></i>
+                                    <i className="fa fa-twitter fa-lg" onClick={() => { window.location.replace("https://x.com/UshanDilusha_") }}></i>
+                                    <i className="fa fa-linkedin-square fa-lg" onClick={() => { window.location.replace("https://www.linkedin.com/in/ushan-dilusha-492b1519b") }}></i>
+                                    <i className="fa fa-github" onClick={() => { window.location.replace("https://github.com/Arutha-Project") }}></i>
                                 </div>
                             </div>
                         </div>
@@ -32,10 +31,10 @@ const Footer = () => {
                         <div className="col-md-2 col-sm-2 col-xs-2">
                             <h5 className="heading">Team Members</h5>
                             <ul>
-                                <li>Ushan Dilusha</li>
-                                <li>Ovinda Namal</li>
-                                <li>Thiwanka Kalpage</li>
-                                <li>Ishara Madushan</li>
+                                <li onClick={() => { window.location.replace("https://www.linkedin.com/in/ushan-dilusha-492b1519b/") }}>Ushan Dilusha</li>
+                                <li onClick={() => { window.location.replace("https://www.linkedin.com/in/j-m-ovinda-namal-663a42252/") }}>Ovinda Namal</li>
+                                <li onClick={() => { window.location.replace("https://www.linkedin.com/in/thiwanka-kalpage/") }}>Thiwanka Kalpage</li>
+                                <li onClick={() => { window.location.replace("https://www.linkedin.com/in/ishara-madushan-3919ab217/") }}>Ishara Madushan</li>
                             </ul>
                         </div>
                         <div className="col-md-2 col-sm-2 col-xs-2">
@@ -50,7 +49,7 @@ const Footer = () => {
                         <div className="col-md-2 col-sm-2 col-xs-2">
                             <h5 className="heading">Contact Us</h5>
                             <ul className="card-text">
-                                <li>SLIIT</li>
+                                <li onClick={() => { window.location.replace("https://www.sliit.lk/") }}>SLIIT</li>
                                 <li>Malabe</li>
                                 <li>Kaduwela</li>
                                 <li>071 5273881</li>
